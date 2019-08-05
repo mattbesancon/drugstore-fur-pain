ac101adc60b2a52101290949ae52cffa36dfbfa5
Matthieu Besan�con <matthieu.besancon@gmail.com>
2019-08-05 21:13:22 +0200

    controllers

--- app/controllers/drugs_controller.rb
+++ app/controllers/drugs_controller.rb
@@ -1,13 +1,29 @@
 class DrugsController < ApplicationController
   def index
+    @drugs = Drug.all
   end
 
   def show
+    @drug = Drug.find(params[:id])
+    @review = Review.new
   end
 
   def new
+    @drug = Drug.new
   end
 
   def create
+    @drug = Drug.new(drug_params)
+    if @drug.save
+      redirect_to drug_path(@drug)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def drug_params
+    params.require(:drug).permit(:name, :content, :price, :photo)
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,18 @@
 class ReviewsController < ApplicationController
   def create
+    @drug = Drug.find(params[:drug_id])
+    @review = Review.new(review_params)
+    @review.drug = @drug
+    if @review.save
+      redirect_to drug_path(@drug)
+    else
+      render "drugs/show"
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
   end
 end
