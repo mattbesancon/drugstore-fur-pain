2a6bad408571d02abd94c4cdb5d440f5249dec83
Matthieu Besan�con <matthieu.besancon@gmail.com>
2019-08-05 22:14:42 +0200

    first round

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,7 +6,7 @@ class ReviewsController < ApplicationController
     if @review.save
       redirect_to drug_path(@drug)
     else
-      render "drugs/show"
+      render 'drugs/show'
     end
   end
 
