7a5066050c9c591c6918a3ea3e930a86e0e9bf3e
Matthieu Besan�con <matthieu.besancon@gmail.com>
2019-08-05 20:53:52 +0200

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'reviews/create'
+  get 'drugs/index'
+  get 'drugs/show'
+  get 'drugs/new'
+  get 'drugs/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/drugs_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class DrugsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get drugs_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get drugs_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get drugs_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get drugs_create_url
+    assert_response :success
+  end
+
+end
