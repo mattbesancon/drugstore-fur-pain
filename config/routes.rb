0aee24ab5456b19ffe485e88a76dcf1dbb665209
Matthieu Besan�con <matthieu.besancon@gmail.com>
2019-12-21 18:48:02 +0100

    edit drug page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'drugs#index'
+  resources :orders, only: [:show, :create]
   resources :drugs, only: [:index, :show, :new, :create, :destroy] do
     resources :reviews, only: [:create]
   end
