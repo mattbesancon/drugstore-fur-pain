7b97aa0c6a74c4e95c75e9839f30da50c80abf88
Matthieu Besan�con <matthieu.besancon@gmail.com>
2020-04-05 15:39:24 +0200

    add the login for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find_by(id: session[:user])
   end
+
+  def logged_in?
+    current_user != nil
+  end
 end
