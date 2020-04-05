ecf3a97392e18e431501b9038b934838512e1eaa
Matthieu Besan�con <matthieu.besancon@gmail.com>
2020-04-05 16:00:49 +0200

    removing before devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user])
-  end
-
-  def logged_in?
-    current_user != nil
-  end
 end
