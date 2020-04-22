9522c4cf97f1e374cd565be46a6151d93cc4f174
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-04-22 11:22:20 +0200

    adding devise back

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_133657) do
+ActiveRecord::Schema.define(version: 2019_12_21_173859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,19 @@ ActiveRecord::Schema.define(version: 2020_04_06_133657) do
     t.index ["drug_id"], name: "index_reviews_on_drug_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   add_foreign_key "orders", "drugs"
+  add_foreign_key "orders", "users"
   add_foreign_key "reviews", "drugs"
 end
