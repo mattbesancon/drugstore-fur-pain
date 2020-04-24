f68967e5047bb2c5cf4b2797a9b01419c2f8d280
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-04-24 11:21:20 +0200

    css

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_scope :user do
-    delete "/users/sign_out" => "devise/sessions#destroy"
-  end
   root to: 'drugs#index'
   resources :orders, only: [:show, :create] do
     resources :payments, only: :new
