96bd0cfbbce910f52b1a355d47302addf1d9dc79
Matthieu Besancon <matthieu.besancon@gmail.com>
2022-02-19 21:11:13 +0100

    payment working with stripe in test mode

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,12 +2,14 @@ class OrdersController < ApplicationController
     def create
         drug = Drug.find(params[:drug_id])
         order = Order.create!(drug: drug, drug_sku: drug.name, amount: drug.price, state: 'pending', user: current_user)
-        
+
+        Stripe.api_key = 'sk_test_8fHHb5UR5q7PXT6j5qkrTtF500YbLvf4wR'
+
         session = Stripe::Checkout::Session.create(
             payment_method_types: ['card'],
             line_items: [{
             name: drug.name,
-            images: [drug.photo_url],
+            images: [drug.photo],
             amount: drug.price_cents,
             currency: 'eur',
             quantity: 1
